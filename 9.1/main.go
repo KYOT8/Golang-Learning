84f3cdaa2605b626c56d536e5b0bfb45f7ca727e
Maciek <maciejadamczyk615@gmail.com>
2026-02-20 23:19:00 +0100

    wycinki i tablice

--- 9.1/main.go
+++ 9.1/main.go
@@ -8,6 +8,8 @@ import (
 // Testy wycinkow, czyli slice'ow, czyli dynamicznych tablic,
 // ktore moga sie rozrastac w trakcie dzialania programu, w przeciwienstwie do zwyklych tablic,
 // ktore maja staly rozmiar i nie moga sie zmieniac
+//
+
 func main() {
 
 	var listagpuNVIDA = []string{"Nvidia GTX 1030", "Nvidia GTX 1060 3GB", "Nvidia GTX 1060 6GB", "Nvidia GTX 1070 8gb", "Nvidia GTX 1080 8GB"}
